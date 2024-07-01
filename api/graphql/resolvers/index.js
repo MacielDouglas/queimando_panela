b6d07208eff57337d3fadc24b11d90baf60bd290
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-07-01 09:10:42 -0300

    Removido opção de Rating

--- api/graphql/resolvers/index.js
+++ api/graphql/resolvers/index.js
@@ -1,12 +1,7 @@
 import { mergeResolvers } from "@graphql-tools/merge";
 import userResolver from "./user.resolver.js";
 import recipeResolver from "./recipe.resolver.js";
-import ratingResolver from "./rating.resolver.js";
 
-const mergedResolvers = mergeResolvers([
-  userResolver,
-  recipeResolver,
-  ratingResolver,
-]);
+const mergedResolvers = mergeResolvers([userResolver, recipeResolver]);
 
 export default mergedResolvers;
--- api/graphql/typeDefs/index.js
+++ api/graphql/typeDefs/index.js
@@ -1,12 +1,7 @@
 import { mergeTypeDefs } from "@graphql-tools/merge";
 import userTypeDef from "./user.typeDef.js";
 import recipeTypeDef from "./recipe.typeDef.js";
-import ratingTypeDef from "./rating.typeDef.js";
 
-const mergedTypeDefs = mergeTypeDefs([
-  userTypeDef,
-  recipeTypeDef,
-  ratingTypeDef,
-]);
+const mergedTypeDefs = mergeTypeDefs([userTypeDef, recipeTypeDef]);
 
 export default mergedTypeDefs;
