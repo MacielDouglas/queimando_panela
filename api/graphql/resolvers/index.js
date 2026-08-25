3c54eb404ee220a70c4e63d622b191b7f0aa9ed1
MacielDouglas <douglasmaciel.dm@gmail.com>
2026-08-25 15:38:25 -0300

    limpa projeto para recomecar do zero

--- api/graphql/resolvers/index.js
+++
@@ -1,7 +0,0 @@
-import { mergeResolvers } from "@graphql-tools/merge";
-import userResolver from "./user.resolver.js";
-import recipeResolver from "./recipe.resolver.js";
-
-const mergedResolvers = mergeResolvers([userResolver, recipeResolver]);
-
-export default mergedResolvers;
--- client/src/components/OnlyAdminPrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
-
-export default function OnlyAdminPrivateRoute() {
-  const user = useSelector((state) => state.auth.user);
-
-  return user && user.isAdmin ? <Outlet /> : <Navigate to="/login" />;
-}
